5d55bbf8eb094746d7b851956727b8b9d43d765e
Hershiv Haria <hershiv@kdaweb.com>
2017-05-13 12:27:33 +0100

    Added some functionality to handle API failures with information to the end user. Also started planning to put a Google Map in the page.

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ var CLIENT_SECRET = "QGGZYCTYNEDC21AZ1I4VTT5BOVNWTKMDCCNOS5C0LJXF3GZE";
 // The latest version of the Foursquare API I'm going to support (i.e. the day I'm writing this).
 var VERSION = "20170513";
 
+// The number of results I want per request.
+var LIMIT = 20
+
 /**
  * My search function, which will contact the foursquare API and then return the response.
  * @param location - The place I want to search.
@@ -19,14 +22,49 @@ function search(location) {
             client_id: CLIENT_ID,
             client_secret: CLIENT_SECRET,
             near: location,
-            v : VERSION
+            v : VERSION,
+            limit: LIMIT
         },
         success: function(data) {
             console.log(data);
+
+            showResults(data);
         },
         error: function (data) {
             console.log("Something went wrong");
             console.log(data);
+
+            handleFailure(data);
         }
     });
 }
+
+/**
+ * Visualise the results returned from Foursquare
+ * @param data
+ */
+function showResults(data) {
+    $("#search-error").hide();
+    $("#map").show();
+}
+
+/**
+ * Manage a failed result from the API, likely can't find the queried location.
+ * @param data
+ */
+function handleFailure(data) {
+    $("#map").hide();
+    $("#search-error").show();
+}
+
+//On ready function - set up the page to work
+$(function(){
+
+    // Hook up the search box and button
+    $("#search").click(function () {
+        var query = $("#query").val();
+        if (query) {
+            search(query);
+        }
+    })
+});
